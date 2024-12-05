820762663891efc57508b0e9f6bdbd5b40aa47fd
eggfriedrice <eggfriedricew.g.o@gmail.com>
2024-12-05 10:34:06 +0000

    🧹 minor cleanup

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -12,7 +12,7 @@
 // const tokenBlacklist = new Set<string>()
 
 // export const authMiddleware = createMiddleware<AppBindings>(async (c, next) => {
-//   const token = getCookie(c, env?.PROFITON_AUTH_COOKIE ?? "")
+//   const token = getCookie(c, env?.AUTH_COOKIE ?? "")
 
 //   if (!token) {
 //     return c.json({ error: "Unauthorized" }, HttpStatusCodes.UNAUTHORIZED)
@@ -22,7 +22,7 @@
 //     const payload = await verify(token, env?.JWT_SECRET ?? "")
 
 //     if (tokenBlacklist.has(token)) {
-//       deleteCookie(c, env?.PROFITON_AUTH_COOKIE ?? "")
+//       deleteCookie(c, env?.AUTH_COOKIE ?? "")
 //       return c.json({ error: "Token is expired" }, HttpStatusCodes.UNAUTHORIZED)
 //     }
 
@@ -30,7 +30,7 @@
 
 //     await next()
 //   } catch {
-//     deleteCookie(c, env?.PROFITON_AUTH_COOKIE ?? "")
+//     deleteCookie(c, env?.AUTH_COOKIE ?? "")
 //     return c.json(
 //       { error: "Invalid or expired token" },
 //       HttpStatusCodes.UNAUTHORIZED
